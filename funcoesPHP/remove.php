fbddc00b461b796b7112badf53db3feda5124581
Generic-nick <nicolinoplays@gmail.com>
2026-03-26 13:35:32 -0300

    remove.php terminado
    código para a função de delete do crud em execução satisfatória

--- funcoesPHP/remove.php
+++ funcoesPHP/remove.php
@@ -1,13 +1,12 @@
 <?php
 include 'connection.php';
 
-if (isset($_GET['deletar_id'])) {
-    $id_para_del = $_GET['deletar_id'];
-    
-$stmt = $conn->prepare("DELETE FROM produtos WHERE id = ?");
+if (isset($_GET['delete_id'])) {
+    $id_para_del = $_GET['delete_id'];
 
-$stmt->bind_param("i", $id_para_deletar);
-
+$stmt = $conn->prepare("DELETE FROM produtos WHERE id_produto = ?");
+$stmt->bind_param("i", $id_para_del);
+$stmt->execute();
 $stmt->close();
 }
 ?>
