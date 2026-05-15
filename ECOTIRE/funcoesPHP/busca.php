05221a2b6a09009837e89bbd27e983ccdeddc763
André Victor <andrevictoroliver2008@gmail.com>
2026-05-15 09:37:28 -0300

    pagina inicial 100% atualizada

--- ECOTIRE/funcoesPHP/busca.php
+++ ECOTIRE/funcoesPHP/busca.php
@@ -10,19 +10,21 @@ if(isset($_GET['busca'])){
         ':busca' => "%$busca%"
     ]);
 
-    $resultados = $stmt->fetchALL(PDO::FETCH_ASSOC);
+    $resultados = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     if($resultados){
         foreach ($resultados as $produto){
             echo 
             "<a href='../pagina-produto-usuario/pagina-produto-usuario.php?produto=".urlencode($produto['nome'])."&id=".$produto['id_produto']."' 
             class='resultado-item' style='color:rgb(43, 109, 77); text-decoration:none;'>
-            <p>".$produto['nome']." - R$ ".$produto['preco']."</p>
+            <div class='resultado-item'>
+                ".$produto['nome']." - R$ ".$produto['preco']."
+            </div>
             </a>";
         } 
     } else {
             echo "Nenhum resultado encontrado";
-        }
+    }
 }
 
 
